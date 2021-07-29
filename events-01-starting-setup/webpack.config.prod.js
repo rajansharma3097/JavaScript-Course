dfe114512a7e2b99f3a5d3a342049d3bf7af0592
RJay Sharma <sharma.rajan3097@gmail.com>
2021-07-29 17:30:15 +0530

    javascript modules and webpack

---
+++ events-01-starting-setup/webpack.config.prod.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    // filename: '[contenthash].js',
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: 'assets/scripts/'
+  },
+  devtool: 'cheap-source-map',
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
+}
